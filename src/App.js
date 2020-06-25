307d7754f1a494bec3d621018bc4ca38e8f4ba6f
edison mamian <edison.mamian@correounivalle.edu.co>
2020-06-25 14:05:31 -0500

    mostrar componentes de una lista dinamicamente

--- src/App.js
+++ src/App.js
@@ -51,22 +51,12 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          >
-            Mi hobbie: Trotar
-          </Person>
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            change={this.nameChangeHandler}
-          />
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-            click={this.switchNameHandler.bind(this, 'Stephany')}
-          />
+          // Mostrar datos de una lista dinamicamente
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name}
+              age={person.age} />
+          })}
         </div>
       );
     } else {
